fa96562c75f405b0fa3b4f52a0193710ed0f7e8f
Muhammad Saad <154901721+saadhuywr@users.noreply.github.com>
2025-01-03 05:37:50 +0000

    variables_done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const account_Id = 786786;
+let account_Email = "admin@example.com";
+var account_Password = "@admin@";
+account_City = "hacker city";
+
+account_City = "hacker city"; //hum bina 
+
+// account_Id = 2;
+
+account_Email = "admin@gmail.com"
+
+console.log(account_Id);
+console.log(account_Email);
+
+console.table([account_Id, account_Email,account_Password, account_City ])
