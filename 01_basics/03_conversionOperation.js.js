bc65923d0f0f98303c3ea36eccac94bbffc79cf5
Muhammad Saad <154901721+saadhuywr@users.noreply.github.com>
2025-01-06 11:38:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js.js
@@ -0,0 +1,33 @@
+// data type check kerny ky lie typeof method use kerty hai
+
+let saad = "25abc";
+let saadArif =  null; // null means 0 
+
+console.log(typeof saad);
+console.log(saad)
+
+// ab me convert kero ga string ko number me
+let arr = Number(saad)
+
+let x = 13;
+
+console.log(typeof arr) 
+
+let ans = arr + x
+
+console.log(typeof (ans))
+console.log(ans) //nan full form is not a number 
+
+// "33" => 33
+// "33abc" => nan // not a number
+// 1 => true ; 0 = false
+
+let name_student = null;
+
+let boolean = Boolean(name_student)
+
+console.log(boolean)
+
+// 1 => true ; 0 = false
+// "" => false
+// "saad" => true
