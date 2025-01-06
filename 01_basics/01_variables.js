9c0791f59e0b94117b22c3c22e92cc9e385141ce
Muhammad Saad <154901721+saadhuywr@users.noreply.github.com>
2025-01-06 09:33:56 +0000

    update variables file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,12 +3,14 @@ let account_Email = "admin@example.com";
 var account_Password = "@admin@";
 account_City = "hacker city";
 
-account_City = "hacker city"; //hum bina 
+
+account_City = "mujhe nahi pta"; //hum bina datatype lie bhi variable declare ker sakty hai
 
 // account_Id = 2;
 
 account_Email = "admin@gmail.com"
 
+// prefer not to use var because of issue in block scope and functional
 console.log(account_Id);
 console.log(account_Email);
 
