c0b2efb75f34693f4aea9d8c5e54fed51b5fe03c
Muhammad Saad <154901721+saadhuywr@users.noreply.github.com>
2025-01-06 09:39:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node js, not browser
+
+console.log(3 + 3), console.log("muhammad saad");
+
+let name = "Muhammad Saad";
+let age = 21;
+let isLoggedIn = false;
+let abc = null; // iska matlab ye hota hai ky ye undefined nahi hai bs filhal isme koi value assing nahi ki hoi
+
+// number => 2 to 53
+// bigint
+// string => "" string ko double quote or single quote dono me likh sakty hai
+// boolean => true / false
+// null => standalone value hai
+// undefined =>
+// symbol =>
